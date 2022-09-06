e697584449466a4354f051b5e0d8c02d81d6f908
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-06 15:54:14 -0500

    se agrego clase automapper y la clase helpers

--- WebApiCine/Controllers/GenerosController.cs
+++ WebApiCine/Controllers/GenerosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebApiCine.Controllers
 {
---
+++ WebApiCine/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,12 @@
+﻿Single AutoMapper;
+
+namespace WebApiCine.Helpers
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+
+        }
+    }
+}
