3182b3a8dbaea24afdba51c400756639c1caa87d
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-12 02:23:45 -0500

    se agrego la validacion del tipoArchivoValidacion

--- WebApiCine/Validaciones/TipoArchivoValidacion.cs
+++ WebApiCine/Validaciones/TipoArchivoValidacion.cs
@@ -17,7 +17,23 @@ namespace WebApiCine.Validaciones
                 tiposValidos = new string[] { "image/jpeg", "image/png", "image/gif" };
             }
         }
-
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            IFormFile formFile = value as IFormFile;
+            if (formFile == null)
+            {
+                return ValidationResult.Success;
+            }
+            if (!tiposValidos.Contains(formFile.ContentType))
+            {
+                return new ValidationResult($"El tipo del archivo debe ser uno de los siguientes:{ string.Join(",",tiposValidos)}");
+            }
+            return ValidationResult.Success;
+        }
 
     }
 }
