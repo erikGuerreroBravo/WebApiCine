2ff5cdfa7622627a533cff07a6ca2d22f2a72fb4
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-13 15:48:36 -0500

    se agregan los atributos a actualziar de una entidad.

--- WebApiCine/DTO/ActorPatchDto.cs
+++ WebApiCine/DTO/ActorPatchDto.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApiCine.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiCine.DTO
 {
     public class ActorPatchDto
     {
+        [Required]
+        [StringLength(120)]
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
     }
 }
