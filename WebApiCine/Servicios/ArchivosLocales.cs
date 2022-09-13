0caed545de22e098e3a00f735a07014fbd0b3ada
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-13 14:05:24 -0500

    se creo la clase que se encarga de almacenar de forma local en el proyecto todos los archivos.

---
+++ WebApiCine/Servicios/ArchivosLocales.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApiCine.Servicios
+{
+    public class ArchivosLocales
+    {
+        public Task BorrarArchivo(string ruta, string contenedor)
+        {
+            return null;    
+        }
+        public Task<string> EditarArchivo(byte[] contenido, string extension, string contenedor, string ruta)
+        {
+            return null;
+        }
+        public Task<string> GuardarArchivo(byte[] contenido, string extension, string contenedor, string contentType)
+        {
+            return null;
+        }
+    }
+}
