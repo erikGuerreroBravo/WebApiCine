ab12bb8d5983c2545855a12636e284e1b7559ad9
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-13 16:43:08 -0500

    establecemos la cantidad maxima de registros en una pagina utilizando  paginacion.

---
+++ WebApiCine/DTO/PaginacionDto.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApiCine.DTO
+{
+    public class PaginacionDto
+    {
+        public int Pagina { get; set; } = 1;
+
+        private int cantidadRegistrosPorPagina = 10;
+        private readonly int cantidadMaximaRegistrosPorPagina = 50;
+
+
+        public int CantidadRegistrosPorPagina {
+            get => cantidadRegistrosPorPagina;
+            set {
+                ///validamos la cantidad maxima de registros por pagina sea  igual a lo establecido
+                cantidadRegistrosPorPagina = (value > cantidadRegistrosPorPagina)? cantidadMaximaRegistrosPorPagina: value;
+            }
+        }
+
+    }
+}
