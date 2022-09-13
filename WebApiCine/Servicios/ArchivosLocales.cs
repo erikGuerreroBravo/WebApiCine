c8a26a3d7750594bc0717975b5cd7d59a72fccb0
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-13 14:45:18 -0500

    se agrega el metodo que se encarga de guardar las fotos en un directorio de archivos locales.

--- WebApiCine/Servicios/ArchivosLocales.cs
+++ WebApiCine/Servicios/ArchivosLocales.cs
@@ -2,6 +2,13 @@
 {
     public class ArchivosLocales : IAlmacenadorArchivos
     {
+        private readonly IWebHostEnvironment env;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public ArchivosLocales(IWebHostEnvironment env, IHttpContextAccessor httpContextAccessor)
+        {
+            this.env = env;
+            this.httpContextAccessor = httpContextAccessor;
+        }
         public Task BorrarArchivo(string ruta, string contenedor)
         {
             return null;    
@@ -10,9 +17,20 @@
         {
             return null;
         }
-        public Task<string> GuardarArchivo(byte[] contenido, string extension, string contenedor, string contentType)
+        public async Task<string> GuardarArchivo(byte[] contenido, string extension, string contenedor, string contentType)
         {
-            return null;
+            var nombreArchivo = $"{Guid.NewGuid()}{extension}";
+            string folder = Path.Combine(env.WebRootPath, contenedor);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            string ruta = Path.Combine(folder, nombreArchivo);
+            await File.WriteAllBytesAsync(ruta,contenido);
+            //establecemos el esquema utilizado https o http y establecemos el nombre del host
+            var urlActual = $"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}";
+            var urlParaBD = Path.Combine(urlActual, contenedor, nombreArchivo).Replace("//", "/");
+            return urlParaBD;
         }
     }
 }
