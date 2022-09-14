42d148c9586a4ee970f425feffb16930c50cd317
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-14 17:34:51 -0500

    se agrega la clase del controlador de peliculas

---
+++ WebApiCine/Controllers/PeliculasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCine.Controllers
+{
+    [ApiController]
+    [Route("api/peliculas")]
+    public class PeliculasController : ControllerBase
+    {
+
+        public PeliculasController()
+        {
+
+        }
+    }
+}
