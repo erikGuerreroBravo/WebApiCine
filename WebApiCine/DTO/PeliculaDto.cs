3345c10f30b98f4f14843b8b9f401772d5e7c6b5
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-14 17:44:20 -0500

    se agrego la entidad peliculaDto

---
+++ WebApiCine/DTO/PeliculaDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiCine.DTO
+{
+    public class PeliculaDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string Titulo { get; set; }
+        public bool EnCines { get; set; }
+        public DateTime FechaEstreno { get; set; }
+        public string Poster { get; set; }
+    }
+}
