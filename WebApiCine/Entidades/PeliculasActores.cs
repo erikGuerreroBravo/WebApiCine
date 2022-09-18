a0ad99d86a80fe1acf99048975a5e84d954afa23
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-18 15:29:25 -0500

    se agrego la entidad PeliculasActores

---
+++ WebApiCine/Entidades/PeliculasActores.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApiCine.Entidades
+{
+    public class PeliculasActores
+    {
+        public int ActorId { get; set; }
+        public int PeliculasId { get; set; }    
+
+        public string Personaje { get; set; }
+        public int Orden { get; set; }
+
+        public Actor Actor { get; set; }
+        public Pelicula Pelicula { get; set; }  
+
+
+
+
+    }
+}
