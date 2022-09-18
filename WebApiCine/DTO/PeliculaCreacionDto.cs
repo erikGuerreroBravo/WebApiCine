5523a08e6074b36a67e93ec5e8dc7a0e210f162b
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-18 01:10:48 -0500

    se agregaron alores y atributos para la clase PeliculaCreacionDto

--- WebApiCine/DTO/PeliculaCreacionDto.cs
+++ WebApiCine/DTO/PeliculaCreacionDto.cs
@@ -1,6 +1,15 @@
-﻿namespace WebApiCine.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiCine.DTO
 {
     public class PeliculaCreacionDto
     {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string Titulo { get; set; }
+        public bool EnCines { get; set; }
+        public DateTime FechaEstreno { get; set; }
+        public string Poster { get; set; }
     }
 }
