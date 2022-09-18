8581d55725f3bc34d3059bf379bbe50588aeebbd
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-18 01:11:58 -0500

    se cambia la extension de la propiedad poster.

--- WebApiCine/DTO/PeliculaCreacionDto.cs
+++ WebApiCine/DTO/PeliculaCreacionDto.cs
@@ -10,6 +10,7 @@ namespace WebApiCine.DTO
         public string Titulo { get; set; }
         public bool EnCines { get; set; }
         public DateTime FechaEstreno { get; set; }
-        public string Poster { get; set; }
+
+        public IFormFile Poster { get; set; }
     }
 }
