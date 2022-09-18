fa9dbd244d1fcb81f52b776178c46e832fa5ff90
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-18 16:16:18 -0500

    se agrego el binder de enlazado  para la transmision de los datos de forma adecuada.

--- WebApiCine/DTO/PeliculaCreacionDto.cs
+++ WebApiCine/DTO/PeliculaCreacionDto.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using WebApiCine.Helpers;
 using WebApiCine.Validaciones;
 
 namespace WebApiCine.DTO
@@ -9,7 +11,9 @@ namespace WebApiCine.DTO
         [SizeImagenValidacion(pesoMaximoMB: 4)]
         [TipoArchivoValidacion(grupoTipoArchivo: GrupoTipoArchivo.Imagen)]
         public IFormFile Poster { get; set; }
+        //es necesario utilizar un modelBinder para poder bindear todas las propiedades dentro del modelo 
         //Ids Asociados de los generos a las peliculas
+        [ModelBinder(BinderType = typeof(TypeBinder))]
         public List<int> GenerosIds { get; set; }
 
 
