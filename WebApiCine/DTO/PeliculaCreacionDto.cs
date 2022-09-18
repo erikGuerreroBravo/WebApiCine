1156e14eb7a8876fb414c00448993dd8dfa5b912
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-18 15:50:03 -0500

    se agregan todos los elementos de la clase

--- WebApiCine/DTO/PeliculaCreacionDto.cs
+++ WebApiCine/DTO/PeliculaCreacionDto.cs
@@ -9,7 +9,7 @@ namespace WebApiCine.DTO
         [SizeImagenValidacion(pesoMaximoMB: 4)]
         [TipoArchivoValidacion(grupoTipoArchivo: GrupoTipoArchivo.Imagen)]
         public IFormFile Poster { get; set; }
-
+        //Ids Asociados de los generos a las peliculas
         public List<int> GenerosIds { get; set; }
 
 
