9e66386576abab9cb6ea68fb97d9fc67262f4553
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-18 23:42:31 -0500

    documentacion de la clase actorPeliculasCreacionDto.

--- WebApiCine/DTO/ActorPeliculasCreacionDto.cs
+++ WebApiCine/DTO/ActorPeliculasCreacionDto.cs
@@ -2,6 +2,7 @@
 {
     public class ActorPeliculasCreacionDto
     {
+        //identificador de la entidad.
         public int ActorId { get; set; }
         public string Personaje { get; set; }
     }
