20fa77efed6a3c2d348cbb1878ef56109c6082ad
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-19 00:25:55 -0500

    se genera la primer función de mapeo

--- WebApiCine/Helpers/AutoMapperProfiles.cs
+++ WebApiCine/Helpers/AutoMapperProfiles.cs
@@ -17,14 +17,24 @@ namespace WebApiCine.Helpers
             CreateMap<Pelicula,PeliculaDto>().ReverseMap();
             CreateMap<PeliculaCreacionDto, Pelicula>()
                 .ForMember(x => x.Poster, options => options.Ignore())
-                .ForMember(x => x.PeliculasGeneros, options => options.MapFrom();
+                .ForMember(x => x.PeliculasGeneros, options => options.MapFrom(MapPeliculasGeneros);
             CreateMap<PeliculaPatchDto,Pelicula>().ReverseMap();    
 
         }
 
         private List<PeliculasGeneros> MapPeliculasGeneros(PeliculaCreacionDto peliculaCreacionDto, Pelicula pelicula)
         {
-            
+            var resultado = new List<PeliculasGeneros>();
+            if(peliculaCreacionDto.GenerosIds == null)
+            {
+                return resultado;
+            }
+            foreach (var id in peliculaCreacionDto.GenerosIds)
+            {
+                resultado.Add(new PeliculasGeneros() { GeneroId = id});
+            }
+            return resultado;
+
         }
 
 
