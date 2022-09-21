bd53978e19c9c38392c3cf4f1c3c8b97d7fdc22b
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-21 00:04:13 -0500

    se crean los metodos get set de la clase FiltroPeliculasDto

--- WebApiCine/DTO/FiltroPeliculasDto.cs
+++ WebApiCine/DTO/FiltroPeliculasDto.cs
@@ -2,5 +2,15 @@
 {
     public class FiltroPeliculasDto
     {
+        public int Pagina { get; set; } = 1;
+        public int CantidadRegistrosPorPagina { get; set; } = 10;
+        public PaginacionDto Paginacion
+        {
+            get
+            {
+                return new PaginacionDto() { Pagina = Pagina, CantidadRegistrosPorPagina = CantidadRegistrosPorPagina };
+
+            }
+        }
     }
 }
