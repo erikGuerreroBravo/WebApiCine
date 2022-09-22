56cd602678b534ffe61daea08d236c559ab01fec
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-22 15:52:16 -0500

    se agrego nuevas propiedades de la clase filtroPeliculasDto

--- WebApiCine/DTO/FiltroPeliculasDto.cs
+++ WebApiCine/DTO/FiltroPeliculasDto.cs
@@ -16,5 +16,9 @@
         public int GeneroId { get; set; }
         public bool EnCines { get; set; }
         public bool ProximosEstrenos { get; set; }
+
+        public string CampoOrdenar { get; set; }
+
+        public bool OrdenAscendente { get; set; }
     }
 }
