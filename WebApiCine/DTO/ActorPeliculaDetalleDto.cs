f29513347b4762131a52d48d4acd250a8fd9b641
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-22 12:41:32 -0500

    se agregan las propiedaades del actorPeliculaDetalleDto

---
+++ WebApiCine/DTO/ActorPeliculaDetalleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiCine.DTO
+{
+    public class ActorPeliculaDetalleDto
+    {
+        public int ActorId { get; set; }
+        public string Personaje { get; set; }
+
+        public string NombrePersona { get; set; }
+    }
+}
