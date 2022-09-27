446fb836e90ac896e17914bf8c116f0e4de63b19
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-26 23:57:08 -0500

    se crea el mapper para el constructor.

--- WebApiCine/Controllers/CustomBaseController.cs
+++ WebApiCine/Controllers/CustomBaseController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiCine.Controllers
 {
     public class CustomBaseController : ControllerBase
     {
         private readonly ApplicationDbContext context;
-        public CustomBaseController(ApplicationDbContext context)
+        private readonly IMapper mapper;
+
+        public CustomBaseController(ApplicationDbContext context, IMapper _mapper)
         {
-            this.context = context; 
+            this.context = context;
+            mapper = _mapper;
         }
+
+        
     }
 }
