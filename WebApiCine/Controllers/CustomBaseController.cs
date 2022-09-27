26d62539de95bdd86c067311057b84c00793eb6f
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-27 02:07:19 -0500

    se genero el metodo put.

--- WebApiCine/Controllers/CustomBaseController.cs
+++ WebApiCine/Controllers/CustomBaseController.cs
@@ -47,7 +47,7 @@ namespace WebApiCine.Controllers
 
         protected async Task<ActionResult> Put<TCreacion, TEntidad>(int id, TCreacion creacionDto) where TEntidad : class, IId
         {
-            var entidad = mapper.Map<Genero>(creacionDto);
+            var entidad = mapper.Map<TEntidad>(creacionDto);
             entidad.Id = id;
             context.Entry(entidad).State = EntityState.Modified;
             await context.SaveChangesAsync();
