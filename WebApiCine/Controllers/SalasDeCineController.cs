9c61240f20e4edebaea715ee3a964d467d9b315b
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-28 02:45:52 -0500

    se agrega el metodo put para el controlador salas de cine

--- WebApiCine/Controllers/SalasDeCineController.cs
+++ WebApiCine/Controllers/SalasDeCineController.cs
@@ -7,23 +7,23 @@ namespace WebApiCine.Controllers
 {
     [Route("api/SalasDeCine")]
     [ApiController]
-    public class SalasDeCineController: CustomBaseController
+    public class SalasDeCineController : CustomBaseController
     {
         private readonly ApplicationDbContext context;
         private readonly IMapper mapper;
-        public SalasDeCineController(ApplicationDbContext _context, IMapper _mapper): base(_context,_mapper)
+        public SalasDeCineController(ApplicationDbContext _context, IMapper _mapper) : base(_context, _mapper)
         {
             this.context = _context;
             this.mapper = _mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<SalaDeCineDto>>> Get() 
+        public async Task<ActionResult<List<SalaDeCineDto>>> Get()
         {
             return await Get<SalaDeCine, SalaDeCineDto>();
         }
         [HttpGet("{id:int}", Name = "obtenerSalaDeCine")]
-        public async Task<ActionResult<SalaDeCineDto>> Get(int id) 
+        public async Task<ActionResult<SalaDeCineDto>> Get(int id)
         {
             return await Get<SalaDeCine, SalaDeCineDto>(id);
         }
@@ -32,7 +32,10 @@ namespace WebApiCine.Controllers
         {
             return await Post<SalaDeCineCreacionDto, SalaDeCine, SalaDeCineDto>(salaDeCineCreacionDto, "obtenerSalaDeCine");
         }
-
-
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody] SalaDeCineCreacionDto salaDeCineCreacionDto)
+        {
+            return await Put<SalaDeCineCreacionDto, SalaDeCine>(id, salaDeCineCreacionDto);
+        }
     }
 }
