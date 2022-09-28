e788363a1a81b9e3254b270e5fb15c745db5ee70
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-28 02:01:01 -0500

    se agrega la entidad PeliculasSalaDeCine

---
+++ WebApiCine/Entidades/PeliculasSalaDeCine.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiCine.Entidades
+{
+    public class PeliculasSalaDeCine
+    {
+        public int PeliculaId { get; set; }
+        public int SalaDeCineId { get; set; }
+        public Pelicula Pelicula { get; set; }
+        public SalaDeCine SalaDeCine { get; set; }
+    }
+}
