a0f499669cc78ff610704b05c19de18825aa217b
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-09-28 01:57:07 -0500

    se agrega el metodo  y propiedades en saladecine

--- WebApiCine/Entidades/SalaDeCine.cs
+++ WebApiCine/Entidades/SalaDeCine.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApiCine.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiCine.Entidades
 {
     public class SalaDeCine
     {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(120)]
+        public string Nombre { get; set; }
     }
 }
